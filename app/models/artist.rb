126f3505aa332c918e7c1c4c036d0364d273e04c
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-24 04:08:11 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,6 +11,7 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    #return the number of genres associated with the artist 
+    self.genres.size
   end
 end
